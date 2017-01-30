7bfc581ee22d4347046018608aef7453a92d3094
Arminas <arminasb7@gmail.com>
2017-01-30 19:40:31 +0000

    Initial commit
    Video section done

--- page-home.php
+++ page-home.php
@@ -186,7 +186,7 @@ get_header(); ?>
 				<div class="row">
 					<div class="col-sm-8 col-sm-offset-2">
 						<h2><?php echo $video_section_title; ?></h2>
-						<iframe width="100%" height="415" src=""<?php echo $video_section; ?>" frameborder="0" allowfullscreen></iframe>
+						<iframe width="100%" height="415" src="<?php echo $video_section; ?>" frameborder="0" allowfullscreen></iframe>
 					</div>
 				</div>
 			</div>
