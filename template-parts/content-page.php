b030bd75fd6053971f348c107263c477da992523
Arminas <arminasb7@gmail.com>
2017-02-01 21:25:29 +0000

    Initial commit

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,33 +11,37 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
 		<?php
-			the_content();
+		if ( is_single() ) :
+			the_title( '<h3 class="entry-title">', '</h3>' );
+		else :
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+		endif;
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'b2w' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		if ( 'post' === get_post_type() ) : ?>
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						esc_html__( 'Edit %s', 'b2w' ),
-						the_title( '<span class="screen-reader-text">"', '"</span>', false )
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+		<div class="post-details">
+			<i class="fa fa-user"></i><?php the_author(); ?>
+			<i class="fa fa-clock-o"></i><?php echo get_the_date(); ?>
+			<i class="fa fa-folder-open"></i><?php the_category(', '); ?>
+			<i class="fa fa-tags"></i><?php the_tags() ?>
+			<div class="post-comments-badge">
+				<a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i><?php comments_number( 0, 1, '%'); ?></a>
+			</div>
+			<?php edit_post_link('Edit', '<i class="fa fa-pencil"></i>'); ?>
+			<!-- /.post-comments-badge -->
+		</div>
+
+		<?php
+		endif; ?>
+	</header><!-- .entry-header -->
+	<?php if (has_post_thumbnail()) { ?>
+	<div class="post-image">
+		<?php the_post_thumbnail(); ?>
+	</div>
+	<?php } ?>
+	<div class="post-body">
+		<?php the_content(); ?>
+
+	</div>
 </article><!-- #post-## -->
